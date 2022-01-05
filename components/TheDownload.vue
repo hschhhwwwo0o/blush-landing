fe00babba22580ef62c6a06abb11fec0bc3b5979
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-05 19:27:30 +0300

    Move links

--- components/TheDownload.vue
+++ components/TheDownload.vue
@@ -1,11 +1,31 @@
 <template>
-  <div class="pt-6 lg:pt-12">
-    <a href="" class="underline text-lg">Download for Linux</a>
+  <div class="pt-6 lg:pt-12 flex flex-row gap-10">
+    <span
+      @click="changeLinksDownloadingVisible"
+      :class="`underline text-lg cursor-pointer transition-opacity duration-500 ${
+        isOpenDownload ? 'opacity-70' : 'opacity-100'
+      }`"
+      >Download for Linux</span
+    >
+    <div v-if="isOpenDownload" class="flex flex-row gap-6 text-lg">
+      <a href="" class="underline"> .deb (x64) </a>
+      <a href="" class="underline"> .rpm (x64) </a>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'TheDownload',
+  data() {
+    return {
+      isOpenDownload: false,
+    }
+  },
+  methods: {
+    changeLinksDownloadingVisible() {
+      this.isOpenDownload = !this.isOpenDownload
+    },
+  },
 }
 </script>

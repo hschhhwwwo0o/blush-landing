97ac3ba869dbd79cebc092b5d75f03f89b75c7f2
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-05 19:32:02 +0300

    thanks page

---
+++ pages/thanks-for-downloading.vue
@@ -0,0 +1,23 @@
+<template>
+  <the-layout>
+    <main>
+      <h1 class="text-3xl lg:text-5xl cormorant-garamond uppercase">
+        Thanks for downloading
+      </h1>
+    </main>
+  </the-layout>
+</template>
+
+<script>
+import TheLayout from '@/components/TheLayout.vue'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    TheLayout,
+  },
+  mounted() {
+    window.location.href = 'http://localhost:3000/'
+  },
+}
+</script>

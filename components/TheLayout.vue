73e956f488bf79c65fd55e8e24107f1e9982346d
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-05 18:37:39 +0300

    Initial layout

---
+++ components/TheLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full flex items-center justify-center px-32 pt-36">
+    <div class="w-full max-w-6xl">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheLayout',
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>Empty</div>
+  <the-layout> Empty </the-layout>
 </template>
 
 <script>
+import TheLayout from '@/components/TheLayout.vue'
+
 export default {
   name: 'IndexPage',
+  components: {
+    TheLayout,
+  },
 }
 </script>

d4e2b1cb79ab43ff6c0715f67c7fafde0ffc4285
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-05 20:30:00 +0300

    Fix download

--- components/TheDownload.vue
+++ components/TheDownload.vue
@@ -1,27 +1,35 @@
 <template>
   <div class="pt-6 lg:pt-12 flex flex-row gap-10">
     <span
-      @click="changeLinksDownloadingVisible"
       :class="`underline text-lg cursor-pointer transition-opacity duration-500 ${
         isOpenDownload ? 'opacity-70' : 'opacity-100'
       }`"
+      @click="changeLinksDownloadingVisible"
       >Download for Linux</span
     >
     <div v-if="isOpenDownload" class="flex flex-row gap-6 text-lg">
-      <a
-        download
-        href="https://drive.google.com/file/d/1SKmHN2ybdK2zPhmOD0VwfYmBQgjoe_ER/view?usp=sharing"
-        class="underline"
+      <span
+        class="underline cursor-pointer"
+        @click="
+          () =>
+            goToDownload(
+              'https://drive.google.com/file/d/1SKmHN2ybdK2zPhmOD0VwfYmBQgjoe_ER/view?usp=sharing'
+            )
+        "
       >
         .deb (x64)
-      </a>
-      <a
-        download
-        href="https://drive.google.com/file/d/1PGfE5NUpYaGm4lLX0OB6K0_VZ4cSALM1/view?usp=sharing"
-        class="underline"
+      </span>
+      <span
+        class="underline cursor-pointer"
+        @click="
+          () =>
+            goToDownload(
+              'https://drive.google.com/file/d/1PGfE5NUpYaGm4lLX0OB6K0_VZ4cSALM1/view?usp=sharing'
+            )
+        "
       >
         .rpm (x64)
-      </a>
+      </span>
     </div>
   </div>
 </template>
@@ -37,6 +45,12 @@ export default {
   methods: {
     changeLinksDownloadingVisible() {
       this.isOpenDownload = !this.isOpenDownload
+    },
+    goToDownload(href) {
+      this.$router.push('/thanks-for-downloading')
+      setTimeout(() => {
+        window.open(String(href))
+      }, 1000)
     },
   },
 }

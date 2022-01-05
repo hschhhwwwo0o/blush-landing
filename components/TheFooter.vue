8c26f9579fe6965a12616d0c54fc40a7b10f6a60
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-05 19:15:11 +0300

    Fix links

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="flex flex-col gap-4 text-lg mt-16 pb-16">
-    <a href="" class="underline">GitHub</a>
-    <a href="" class="underline">Behance</a>
-    <a href="" class="underline">Dribbble</a>
-    <a href="" class="underline">Tretyakov Gallery</a>
-    <a href="" class="underline">Giga Tamarashvili</a>
+    <a href="https://github.com/ssandry/blush" class="underline">GitHub</a>
+    <a
+      href="https://www.behance.net/gallery/120582893/BLUSH-Music-App"
+      class="underline"
+      >Behance</a
+    >
+    <a
+      href="https://dribbble.com/shots/15761581-BLUSH-Music-App"
+      class="underline"
+      >Dribbble</a
+    >
+    <a href="https://www.tretyakovgallery.ru/" class="underline"
+      >Tretyakov Gallery</a
+    >
+    <a href="https://dribbble.com/Tamarashvili" class="underline"
+      >Giga Tamarashvili</a
+    >
   </div>
 </template>
 
